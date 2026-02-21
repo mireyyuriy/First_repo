fb4110e8f8f990aad56f98f69e932a302d885c61
mireyyuriy <78160545+mireyyuriy@users.noreply.github.com>
2026-02-21 13:50:03 +0200

    added boolean function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("Hello World")
 
 print("Hello Git")
+
+def is_adult(age):
+    return age >= 18
+
+# Виклик функції, який поверне True або False
+can_vote = is_adult(20) 
+print(can_vote)  # Виведе: True
