3bc48f079da74f84a1ff476617ee548bbf62098d
mireyyuriy <78160545+mireyyuriy@users.noreply.github.com>
2026-02-21 13:55:05 +0200

    added for loops

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ def is_adult(age):
 # Виклик функції, який поверне True або False
 can_vote = is_adult(20) 
 print(can_vote)  # Виведе: True
+
+menu = ["wraps", "sandwiches", "soup", "salad"]
+print("Our menu:")
+for item in menu:
+    print(item)
+
+user_profile = {"name": "Wendy", "status": "active"}
+print("\nUser data:")
+for key in user_profile:
+    print(f"{key}: {user_profile[key]}")
